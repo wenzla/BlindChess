ffe5154375cc35878398a624bf3d90c8651e3a07
Allen Wenzl <allenliwenzl@gmail.com>
2017-10-27 11:26:33 -0400

    Rearrandged app so it doesn't succ
    i failed spelling in high shcool

---
+++ myApp/app/src/main/java/io/github/wenzla/testapp/MultiplayerGame.java
@@ -0,0 +1,24 @@
+package io.github.wenzla.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Allen on 10/26/2017.
+ */
+
+
+public class MultiplayerGame extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multiplayer_setup);
+
+    }
+
+}
+
+
